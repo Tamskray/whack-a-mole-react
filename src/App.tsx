f28e07f035a836413bf7643c8b6cf1c324e986ff
tamskray <tamskray25@gmail.com>
2024-06-07 12:06:13 +0300

    improve bomb functionality and effects

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import mole from "./assets/images/mole.png";
 import bomb from "./assets/images/bomb.png";
 import hummer from "./assets/images/hummer1.png";
 import hummerAttack from "./assets/images/hummer2.png";
+import heart from "./assets/images/heart.png";
 
 import bonkSound from "./assets/sounds/bonk.mp3";
 import scoreSound from "./assets/sounds/score.mp3";
+import explosionSound from "./assets/sounds/explosion.mp3";
 
 interface Mole {
   isVisible: boolean;
@@ -22,9 +24,11 @@ function App() {
   const [score, setScore] = useState(0);
   const [cursor, setCursor] = useState<string>(hummer);
   const [playerHealth, setPlayerHealth] = useState<number>(3);
+  const [gameStarted, setGameStarted] = useState<boolean>(false);
 
   const bonkAudio = new Audio(bonkSound);
   const scoreAudio = new Audio(scoreSound);
+  const explosionAudio = new Audio(explosionSound);
 
   function handleHummerAttack() {
     bonkAudio.play();
@@ -53,8 +57,12 @@ function App() {
     setMoleVisibility(index, false);
 
     if (mole.isBomb) {
-      console.log("You hit a bomb");
+      explosionAudio.play();
       setPlayerHealth((health) => health - 1);
+      document.documentElement.classList.add("bomb-hit");
+      setTimeout(() => {
+        document.documentElement.classList.remove("bomb-hit");
+      }, 700);
     } else {
       setScore((score) => score + 1);
       setTimeout(() => {
@@ -63,8 +71,14 @@ function App() {
     }
   }
 
+  function restartGame() {
+    setMoles(Array(9).fill({ isVisible: false, isBomb: false }));
+    setScore(0);
+    setPlayerHealth(3);
+  }
+
   useEffect(() => {
-    if (playerHealth === 0) return;
+    if (playerHealth === 0 || !gameStarted) return;
 
     const moleInterval = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * moles.length);
@@ -77,10 +91,10 @@ function App() {
     return () => {
       clearInterval(moleInterval);
     };
-  }, [moles, playerHealth]);
+  }, [moles, playerHealth, gameStarted]);
 
   useEffect(() => {
-    if (playerHealth === 0) return;
+    if (playerHealth === 0 || !gameStarted) return;
 
     const bombInterval = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * moles.length);
@@ -93,32 +107,48 @@ function App() {
     return () => {
       clearInterval(bombInterval);
     };
-  }, [playerHealth]);
-
-  useEffect(() => {
-    console.log(playerHealth);
-  }, [playerHealth]);
+  }, [playerHealth, gameStarted]);
 
   return (
     <>
-      <h1>Score {score}</h1>
-      <h1>HP: {playerHealth}</h1>
-      {playerHealth === 0 && <h1>GG. YOU'R LOSE</h1>}
-      <div
-        className="grid"
-        style={{ cursor: `url(${cursor}), auto` }}
-        onClick={handleHummerAttack}
-      >
-        {moles.map((isMole, idx) => (
-          <img
-            key={idx}
-            src={isMole.isVisible ? (isMole.isBomb ? bomb : mole) : hole}
-            onClick={() => {
-              whackMole(idx);
-            }}
-          />
-        ))}
-      </div>
+      {!gameStarted ? (
+        <div className="game-start-screen">
+          <h1>WHACK A MOLE</h1>
+          <button onClick={() => setGameStarted(true)}>PLAY</button>
+        </div>
+      ) : (
+        <div className="game-container">
+          <h1>Score: {score}</h1>
+
+          <div className="player-health">
+            <img className="heart" src={heart} />
+            <h2>x {playerHealth}</h2>
+          </div>
+
+          {playerHealth === 0 && (
+            <div>
+              <h2>GG. YOU'R LOSE</h2>
+              <button onClick={restartGame}>RESTART</button>
+            </div>
+          )}
+
+          <div
+            className="grid"
+            style={{ cursor: `url(${cursor}), auto` }}
+            onClick={handleHummerAttack}
+          >
+            {moles.map((isMole, idx) => (
+              <img
+                key={idx}
+                src={isMole.isVisible ? (isMole.isBomb ? bomb : mole) : hole}
+                onClick={() => {
+                  whackMole(idx);
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
